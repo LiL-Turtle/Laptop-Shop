b11bbeeb9bcd6670b06d85f37e3846b1c8ef8c0d
Bảo Thiên <baothien9900@gmail.com>
2024-07-03 13:59:22 +0700

    modelAttribute + method='POST'

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,15 +1,13 @@
 package vn.hoidanit.laptopshop.controller;
 
-import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
-
-import org.springframework.web.bind.annotation.GetMapping;  
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -19,30 +17,26 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
-    
+
     @RequestMapping("/")
-    public String getHomePage(Model model){
+    public String getHomePage(Model model) {
         String test = this.userService.handleHello();
         model.addAttribute("thien", test);
         model.addAttribute("turtle", "from controller with model");
         return "hello";
     }
+
+    @RequestMapping("admin/user")
+    public String getUserPage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("newUser", new User());
+        model.addAttribute("turtle", "from controller with model");
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "admin/user/create", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User baothien) {
+        System.out.println("Run here" + baothien);
+        return "hello";
+    }
 }
-
-
-// @RestController
-// public class UserController {
-
-//     //DI: dependency injection
-//     private UserService userService;
-
-//     public UserController(UserService userService) {
-//         this.userService = userService;
-//     }
-
-
-//     @GetMapping("/")
-//     public String getHomePage() {
-//         return this.userService.handleHello();
-//     }
-// }
